c358387f96d8d195087164ce483c72cb07eef267
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-04-06 23:12:30 +0200

    title changed

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+
+import { useState, useEffect } from "react";
+
+
+const App = () => {
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState(0);
+
+  useEffect(() => {
+    console.log("First updated: ", first);
+  }, [first]);
+
+  useEffect(() => {
+    console.log("Second updated: ", second);
+  }, [second]);
+
+  useEffect(() => {
+    console.log("First or second updated: ", first + second);
+  }, [first, second]);
+
+  return (
+    <>
+      <button onClick={() => setFirst(first + 1)}>First: {first}</button>
+      <button onClick={() => setSecond(second + 1)}>Second: {second}</button>
+    </>
+  );
+};
+
+
+export default App
