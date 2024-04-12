8ab809cbd18538b7ceb0af1f8c944f01353b2a4d
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-04-12 15:51:24 +0200

    up state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,34 @@ import Feedback from "./feedback/Feedback";
 import Options from "./options/Options";
 
 const App = () => {
-  // const [opinions, setOpinions] = useState({
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // });
+  const [option, setOpions] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  function clickHandler(e) {
+    let type = e.currentTarget.name;
+    switch (type) {
+      case "good":
+        setOpions({ ...option, good: option.good + 1 });
+        break;
+      case "neutral":
+        setOpions({ ...option, neutral: option.neutral + 1 });
+        break;
+      case "bad":
+        setOpions({ ...option, bad: option.bad + 1 });
+        break;
+      case "reset":
+        setOpions({ good: 0, neutral: 0, bad: 0 });
+        break;
+    }
+  }
 
   return (
     <div>
       <Description></Description>
-      <Options></Options>
+      <Options handler={clickHandler} value={option} ></Options>
       <Feedback></Feedback>
     </div>
   );
--- src/components/options/Options.jsx
+++ src/components/options/Options.jsx
@@ -1,45 +1,22 @@
-import { useState } from "react";
+export default function Feedback({ handler, value }) {
+  function Button({ value, name, handler }) {
+    console.log(value);
+    console.log(handler);
+    console.log(name);
+   
 
-export default function Feedback() {
-  const [option, setOpions] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
-
-  function clickHandler(e) {
-    let type = e.currentTarget.name;
-    switch (type) {
-      case "good":
-        setOpions({ ...option, good: option.good + 1 });
-        break;
-      case "neutral":
-        setOpions({ ...option, neutral: option.neutral + 1 });
-        break;
-      case "bad":
-        setOpions({ ...option, bad: option.bad + 1 });
-        break;
-      case "reset":
-        setOpions({ good: 0, neutral: 0, bad: 0 });
-        break;
-    }
+    return (
+      <button onClick={handler} name={name}>
+        {name}
+      </button>
+    );
   }
 
   return (
     <div>
-      {/* <Button type="Good" function={raseClick}></Button> */}
-      <button onClick={clickHandler} name="good">
-        Good:{option.good}
-      </button>
-      <button onClick={clickHandler} name="neutral">
-        Neutral:{option.neutral}
-      </button>
-      <button onClick={clickHandler} name="bad">
-        Bad:{option.bad}
-      </button>
-      <button onClick={clickHandler} name="reset">
-        Reset
-      </button>
+      <Button handler={handler} value={value} name="good"></Button>
+      {/* <Button name="Bad"></Button>
+      <Button name="Neutral"></Button> */}
     </div>
   );
 }
